8322efcfced93bc6d2771a676aba9b76f743422f
lazyKT <kyaw.thitlwin.me@gmail.com>
2021-06-17 10:22:42 +0800

    Layout Updates

---
+++ app/src/main/java/com/example/singbike/Fragments/AccountTab/RideHistoryFragment.java
@@ -0,0 +1,11 @@
+package com.example.singbike.Fragments.AccountTab;
+
+import androidx.fragment.app.Fragment;
+
+public class RideHistoryFragment extends Fragment {
+
+    public RideHistoryFragment () {
+        super();
+    }
+
+}
