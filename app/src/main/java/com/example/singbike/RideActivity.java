8f849dcf948ab8968f8aeacf6208a3f198d7b50a
lazyKT <kyaw.thitlwin.me@gmail.com>
2021-08-07 12:01:57 +0800

    Live Location Requests and Updates on Foreground.

--- app/src/main/java/com/example/singbike/RideActivity.java
+++ app/src/main/java/com/example/singbike/RideActivity.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,29 +15,31 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import com.example.singbike.Dialogs.ErrorDialog;
-import com.example.singbike.Models.Ride;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
 import java.util.Locale;
 
-public class RideActivity extends AppCompatActivity
-        implements OnMapReadyCallback, GoogleMap.OnCameraMoveStartedListener, GoogleMap.OnCameraMoveListener {
+public class RideActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private final String DEBUG_RIDE_TiME = "DEBUG_RIDE_TiME";
-    private boolean isLocationGranted;
+    private static final String DEBUG_RIDE_STATE = "DEBUG_ACTIVITY_STATE";
+    private static final String DEBUG_RIDE_LOCATION = "DEBUG_RIDE_LOCATION";
+    private static final String DEBUG_RIDE_TiME = "DEBUG_RIDE_TiME";
+    private static final String REQUESTING_LOCATION_UPDATES_KEY = "LOCATION_UPDATE_KEYS";
+    private boolean isLocationGranted, requestingLocationUpdates, isForeGround;
     private Location lastKnownLocation;
     private FusedLocationProviderClient fusedLocationProviderClient;
     private TextView timeTextView;
@@ -45,17 +48,30 @@ public class RideActivity extends AppCompatActivity
     private Runnable runnable;
     private GoogleMap map;
 
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
+
     @Override
     public void onCreate(Bundle savedInstanceStates) {
         super.onCreate(savedInstanceStates);
         setContentView(R.layout.activity_ride);
 
+        Log.d (DEBUG_RIDE_STATE, "Inside onCreate Method!");
+
+        isForeGround = true;
+
         Intent intent = getIntent();
         isLocationGranted = intent.getBooleanExtra("isLocationGranted", false);
+
+        updateValuesFromBundles (savedInstanceStates);
+        Log.d (DEBUG_RIDE_STATE, "From savedInstanceStates (Requesting Location) : " + requestingLocationUpdates);
+
+        requestingLocationUpdates = true;
 
         if (!isLocationGranted) requestLocationPermission();
 
         startTime = System.currentTimeMillis();
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient (RideActivity.this);
 
         Button finishRideButton = findViewById(R.id.finishRide_Button);
         timeTextView = findViewById(R.id.timeTextView_Ride);
@@ -65,25 +81,64 @@ public class RideActivity extends AppCompatActivity
         /* start timer to record the ride time */
         startTimer();
 
+        /* create and configure location request to update the user routes during the ride  */
+        createLocationRequest ();
+
+        /* instantiate locationCallBack */
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult (@NonNull LocationResult result) {
+//                Log.d (DEBUG_RIDE_LOCATION, "onLocationResult: Before 'for' loop");
+                for (Location location : result.getLocations()) {
+                    Log.d (DEBUG_RIDE_LOCATION, "onLocationResult: inside 'for' loop" + location.toString());
+                    updateLocationOnUI (location);
+                }
+            }
+        };
+
         finishRideButton.setOnClickListener(v -> {
             // cancel the timer runnable
             handler.removeCallbacks(runnable);
+            requestingLocationUpdates = false;
             startActivity(new Intent(RideActivity.this, MainActivity.class));
         });
 
     }
 
     @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
+    public void onResume () {
+        super.onResume();
+        Log.d (DEBUG_RIDE_STATE, "Inside onResume Method!");
+        if (requestingLocationUpdates)
+            startLocationRequest();
+    }
 
+    @Override
+    public  void onPause () {
+        super.onPause();
+        Log.d (DEBUG_RIDE_STATE, "Inside onPause Method!");
+        isForeGround = false;
+        /* if the activity goes into background, stop the foreground location updates */
+        stopLocationUpdates ();
+    }
+
+    @Override
+    public void onSaveInstanceState (@NonNull Bundle outState) {
+        Log.d (DEBUG_RIDE_STATE, "Inside onSaveInstanceState Method!");
+        outState.putBoolean (REQUESTING_LOCATION_UPDATES_KEY, requestingLocationUpdates);
+        super.onSaveInstanceState (outState);
+    }
+
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        Log.d (DEBUG_RIDE_STATE, "Inside onMapReady Method!");
         this.map = googleMap;
 
         updateMapUI();
-        getDeviceLocation();
 
-        LatLng rafflesPlace = new LatLng(1.2830, 103.8513); // somewhere near Raffles Place MRT
-        // new CameraPosition(target, zoom, tilt, bearing)
-        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition(rafflesPlace, 20, 45, 45)));
+        /* get Device Location and show the position on map */
+        getDeviceLocation();
     }
 
     /* initiate map */
@@ -101,16 +156,6 @@ public class RideActivity extends AppCompatActivity
                 .commit();
 
         mapFragment.getMapAsync(this);
-    }
-
-    @Override
-    public void onCameraMoveStarted(int i) {
-
-    }
-
-    @Override
-    public void onCameraMove() {
-
     }
 
     /* start timer */
@@ -157,9 +202,7 @@ public class RideActivity extends AppCompatActivity
 
     /* launch location permission request */
     private final ActivityResultLauncher<String> requestLocationLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(),
-            isGranted -> {
-                isLocationGranted = isGranted;
-            }
+            isGranted -> isLocationGranted = isGranted
     );
 
 
@@ -170,9 +213,6 @@ public class RideActivity extends AppCompatActivity
 
         try {
             if (isLocationGranted) {
-                map.setOnCameraMoveStartedListener(this);
-                map.setOnCameraMoveListener(this);
-
                 map.setMyLocationEnabled(true);
                 map.getUiSettings().setZoomControlsEnabled(false);
                 map.getUiSettings().setMyLocationButtonEnabled(true);
@@ -218,6 +258,39 @@ public class RideActivity extends AppCompatActivity
         catch (SecurityException se) {
             displayErrorDialog (se.getMessage());
         }
+    }
+
+    private void createLocationRequest () {
+        Log.d (DEBUG_RIDE_LOCATION, "Creating Location Request!");
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval (10000); // make location request every 10 seconds
+        locationRequest.setFastestInterval (5000); // make nearly accurate location updates every 5 seconds
+        locationRequest.setPriority (LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    private void startLocationRequest () {
+        try {
+            Log.d (DEBUG_RIDE_LOCATION, "Starting Location Request!");
+            fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
+        }
+        catch (SecurityException e) {
+            displayErrorDialog (e.getMessage());
+        }
+    }
+
+    private void stopLocationUpdates () {
+        Log.d (DEBUG_RIDE_LOCATION, "Stopping Location Request!");
+        fusedLocationProviderClient.removeLocationUpdates (locationCallback);
+    }
+
+    private void updateValuesFromBundles (Bundle savedInstanceState) {
+        if (savedInstanceState == null)
+            return;
+        requestingLocationUpdates = savedInstanceState.getBoolean (REQUESTING_LOCATION_UPDATES_KEY);
+    }
+
+    private void updateLocationOnUI (Location location) {
+        Log.d (DEBUG_RIDE_LOCATION, "Updating Location on UI " + location.toString());
     }
 
     /* display error message */
